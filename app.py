ce2da704b155a369e71373244e3e91e1b949f764
alanbilla420 <alanbilla2023@gmail.com>
2024-05-30 09:08:09 +0530

    aa

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from chat import get_response
-from flask import Flask,redirect, session, flash
+from flask import Flask, redirect, session, flash
 from flask_mysqldb import MySQL
 import bcrypt
 app = Flask(__name__)
@@ -13,9 +13,12 @@ app.config['MYSQL_PASSWORD'] = '123456'
 app.config['MYSQL_DB'] = 'game'
 mysql = MySQL(app)
 
+
 @app.route('/')
 def home():
     return render_template('login.html')
+
+
 @app.route('/signup', methods=['POST'])
 def signup():
     if request.method == 'POST':
@@ -23,18 +26,21 @@ def signup():
         email = request.form['email']
         password = request.form['password']
         # Hash the password
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        hashed_password = bcrypt.hashpw(
+            password.encode('utf-8'), bcrypt.gensalt())
         # Insert user into database
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", (name, email, hashed_password))
+        cur.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)",
+                    (name, email, hashed_password))
         mysql.connection.commit()
         cur.close()
         flash("Signup successful", "success")
         return redirect('/')
 
+
 @app.route('/signin', methods=['POST'])
 def signin():
-     if request.method == 'POST':
+    if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
         cur = mysql.connection.cursor()
@@ -48,27 +54,43 @@ def signin():
         else:
             flash("Invalid email or password", "danger")
             return redirect('/')
+
+
 @app.get("/dashboard")
 def index_get():
-     return render_template("index.html")
+    return render_template("index.html")
+
+
 @app.get("/ex")
 def index_getex():
-     return render_template("base.html")
+    return render_template("base.html")
+
+
 @app.route('/blog')
 def block():
     return render_template('blog.html')
+
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
 @app.route('/game-single')
 def gameSingle():
     return render_template('game-single.html')
+
+
 @app.route('/games')
 def games():
     return render_template('games.html')
+
+
 @app.route('/review')
 def review():
     return render_template('review.html')
+
+
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
@@ -78,5 +100,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)  
- 
+    app.run(debug=True)
--- chat.py
+++ chat.py
@@ -1,13 +1,16 @@
+from nltk_utils import bag_of_words, tokenize
+from model import NeuralNet
 import random
 import json
 import MySQLdb
 import pyodbc
 import torch
-from flask import Flask,redirect, session, flash
+from flask import Flask, redirect, session, flash
 from flask import Flask, render_template, request, redirect, session
 from flask_mysqldb import MySQL
 import bcrypt
 app = Flask(__name__)
+
 
 def get_tabledata(tag):
     try:
@@ -20,7 +23,7 @@ def get_tabledata(tag):
         return f"Error occurred: {e}"
     finally:
         cursor.close()
- 
+
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -28,6 +31,8 @@ app.config['MYSQL_PASSWORD'] = '123456'
 app.config['MYSQL_DB'] = 'game'
 
 mysql = MySQL(app)
+
+
 def InsertData(UserWants):
     try:
         cursor = mysql.connection.cursor()
@@ -40,10 +45,9 @@ def InsertData(UserWants):
     finally:
         cursor.close()
 
+
 app = Flask(__name__)
 
-from model import NeuralNet
-from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -66,6 +70,7 @@ model.eval()
 
 bot_name = "End Game"
 
+
 def get_response(msg):
     sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
@@ -79,25 +84,25 @@ def get_response(msg):
     prob = probs[0][predicted.item()]
     if prob.item() > 0.75:
         for intent in intents['intents']:
-            if tag == intent["tag"]:                   
+            if tag == intent["tag"]:
                 if tag == "ContactUs":
-                    #return "Test...!"
-                   result1 = get_tabledata(1) 
-                   result2 = get_tabledata(2)      
-                   return result1+" "+result2
+                    # return "Test...!"
+                    result1 = get_tabledata(1)
+                    result2 = get_tabledata(2)
+                    return result1+" "+result2
                 elif tag == "services":
-                   result1 = get_tabledata(3) 
-                   result2 = get_tabledata(4)
-                   result3 = get_tabledata(5) 
-                   result4 = get_tabledata(6)   
-                   result5 = get_tabledata(7) 
-                   result6 = get_tabledata(8)   
-                   result7 = get_tabledata(9) 
-                   result8 = get_tabledata(10)
-                   result9 = get_tabledata(11)
-                   return result1+" "+result2+" "+result3+" "+result4+" "+result5+" "+result6+" "+result7+" "+result8+" "+result9
-                elif tag == "Locations":                                  
-                    result1 = get_tabledata(12)                       
+                    result1 = get_tabledata(3)
+                    result2 = get_tabledata(4)
+                    result3 = get_tabledata(5)
+                    result4 = get_tabledata(6)
+                    result5 = get_tabledata(7)
+                    result6 = get_tabledata(8)
+                    result7 = get_tabledata(9)
+                    result8 = get_tabledata(10)
+                    result9 = get_tabledata(11)
+                    return result1+" "+result2+" "+result3+" "+result4+" "+result5+" "+result6+" "+result7+" "+result8+" "+result9
+                elif tag == "Locations":
+                    result1 = get_tabledata(12)
                     result2 = get_tabledata(13)
                   # print(result1)
                     return (result1+"/t"+result2)
@@ -111,10 +116,9 @@ def get_response(msg):
                 elif tag == "OurPromises":
                     result1 = get_tabledata(17)
                     return result1
-                return random.choice(intent['responses'])     
+                return random.choice(intent['responses'])
         result = InsertData(msg)
     return result
-
 
 
 if __name__ == "__main__":
@@ -123,9 +127,7 @@ if __name__ == "__main__":
         # sentence = "do you use credit cards?"
         sentence = input("You: ")
         if sentence == "quit":
-            break      
+            break
 
         resp = get_response(sentence)
         print(resp)
-
-
